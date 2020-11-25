5dbfbe1c69334e8207ed549e59720833e136ea80
mentoringflutterihsan <73271232+mentoringflutterihsan@users.noreply.github.com>
2020-11-25 14:29:30 +0700

    Update 2020_11_03_124544_create_kecamatans_table.php

--- database/migrations/2020_11_03_124544_create_kecamatans_table.php
+++ database/migrations/2020_11_03_124544_create_kecamatans_table.php
@@ -14,7 +14,7 @@ class CreateKecamatansTable extends Migration
     public function up()
     {
         Schema::create('kecamatans', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->timestamps();
             $table->string('nama_kecamatan');
             $table->integer('luas_wilayah');
